4c450ff254d455694b9afea648f3180537256cc3
teoZH <killdisk13@gmail.com>
2022-02-06 17:45:48 +0200

    added swagger

--- kanban_reworked/settings.py
+++ kanban_reworked/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'base_api',
     'auth_api',
     'base_page_app',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
@@ -121,16 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
